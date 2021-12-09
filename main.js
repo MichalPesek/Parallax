a8c45a34adbd5aed5c388b68c85e5657293e9cf4
Michal Pešek <59620950+MichalPesek@users.noreply.github.com>
2021-12-09 20:09:58 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const parallax = document.getElementById("parallax")
+const h2 = document.getElementById("h2")
 window.addEventListener("scroll", function()
 {
     let offset = window.pageYOffset;
-    console.log('Offset: ' + offset);
+    console.log('offset: ' + offset);
     console.log('offset * 0.7' + offset * 0.7);
     parallax.style.backgroundPositionY = offset * 0.7 + "px";
+    h2.style.backgroundPositionY = offset * 0.6 + "px";
 })
